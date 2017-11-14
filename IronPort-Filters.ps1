38b9e7eadd55d78fdeb2a57ce8dc74cab8de2763
yonivr <33428475+yonivr@users.noreply.github.com>
2017-11-14 08:49:06 +0200

    Create IronPort-Filters.ps1

---
+++ IronPort-Filters.ps1
@@ -0,0 +1,12 @@
+#quarantine_spoofed_protected_senders:
+#Block reciving email from external sources like gmail with identical display names as in the organization
+
+#ironport-il
+quarantine_spoofed_protected_senders: if ((mail-from-dictionary-match("Protected_User_Names", 1)) OR (header-dictionary-match("Protected_User_Names","From", 1))) AND ((sendergroup != "WHITELIST") AND (sendergroup != "DLP") AND (sendergroup != "RELAYLIST") AND (sendergroup != "Authorized_888Holdings")) {
+                                 quarantine("Ext_888Holdings_Spoof");
+                             }
+
+#ironport-lo
+quarantine_spoofed_protected_senders: if ((mail-from-dictionary-match("Protected_User_Names", 1)) OR (header-dictionary-match("Protected_User_Names","From", 1))) AND ((sendergroup != "WHITELIST") AND (sendergroup != "RELAYLIST") AND (sendergroup != "Authorized_888Holdings")) {
+                                 quarantine("Ext_888Holdings_Spoof");
+                             }
