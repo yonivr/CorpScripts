42143d5215989f57829919935060c3a50b62bd2e
yonivr <33428475+yonivr@users.noreply.github.com>
2017-11-18 04:06:53 +0200

    Update get-CPUPerEsx.ps1

--- get-CPUPerEsx.ps1
+++ get-CPUPerEsx.ps1
@@ -1,3 +1,4 @@
+#cpu count
 $result = @()
 $vmhost =  Get-VMHost | ?{$_.ConnectionState -eq "Connected"}
 foreach ($esxi in $vmhost) {
@@ -10,3 +11,11 @@ foreach ($esxi in $vmhost) {
     $result += $obj
 }
 $result
+
+
+#host hardware
+Get-VMHost |Sort Name |Get-View |
+Select Name, 
+@{N=“Type“;E={$_.Hardware.SystemInfo.Vendor+ “ “ + $_.Hardware.SystemInfo.Model}},
+@{N=“CPU“;E={“PROC:“ + $_.Hardware.CpuInfo.NumCpuPackages + “ CORES:“ + $_.Hardware.CpuInfo.NumCpuCores + “ MHZ: “ + [math]::round($_.Hardware.CpuInfo.Hz / 1000000, 0)}},
+@{N=“MEM“;E={“” + [math]::round($_.Hardware.MemorySize / 1GB, 0) + “ GB“}} | Export-Csv c:\hostinfo.csv
