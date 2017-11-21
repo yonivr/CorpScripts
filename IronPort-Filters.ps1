1c0915ddcc63d0bdf608cac37a7233855dfab244
yonivr <33428475+yonivr@users.noreply.github.com>
2017-11-21 11:55:58 +0200

    Update IronPort-Filters.ps1

--- IronPort-Filters.ps1
+++ IronPort-Filters.ps1
@@ -5,6 +5,12 @@
 quarantine_spoofed_protected_senders: if ((mail-from-dictionary-match("Protected_User_Names", 1)) OR (header-dictionary-match("Protected_User_Names","From", 1))) AND ((sendergroup != "WHITELIST") AND (sendergroup != "DLP") AND (sendergroup != "RELAYLIST") AND (sendergroup != "Authorized_888Holdings")) {
                                  quarantine("Ext_888Holdings_Spoof");
                              }
+                             
+quarantine_spoofed_protected_senders: if ((mail-from-dictionary-match("Protected_User_Names", 1)) OR (header-dictionary-match("Protected_User_Names","From", 1))) AND ((sendergroup != "WHITELIST") AND (sendergroup != "DLP") AND (sendergroup != "RELAYLIST") AND (sendergroup != "Authorized_888Holdings") AND (sendergroup != "WHITELIST_External") AND mail-from-dictionary-match("Safe_Display_Name_Impersonators", 1)) {
+notify-copy ("$EnvelopeSender", "", "", "Recipient") ;                               
+quarantine("Ext_888Holdings_Spoof");
+                                         }
+
 
 #ironport-lo
 quarantine_spoofed_protected_senders: if ((mail-from-dictionary-match("Protected_User_Names", 1)) OR (header-dictionary-match("Protected_User_Names","From", 1))) AND ((sendergroup != "WHITELIST") AND (sendergroup != "RELAYLIST") AND (sendergroup != "Authorized_888Holdings")) {
