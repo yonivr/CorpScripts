79186332d97f6d6827f142c9f1e9851f84608d5a
yonivr <33428475+yonivr@users.noreply.github.com>
2017-11-21 13:56:26 +0200

    Update IronPort-Filters.ps1

--- IronPort-Filters.ps1
+++ IronPort-Filters.ps1
@@ -6,8 +6,8 @@ quarantine_spoofed_protected_senders: if ((mail-from-dictionary-match("Protected
                                  quarantine("Ext_888Holdings_Spoof");
                              }
                              
-quarantine_spoofed_protected_senders: if ((mail-from-dictionary-match("Protected_User_Names", 1)) OR (header-dictionary-match("Protected_User_Names","From", 1))) AND ((sendergroup != "WHITELIST") AND (sendergroup != "DLP") AND (sendergroup != "RELAYLIST") AND (sendergroup != "Authorized_888Holdings") AND (sendergroup != "WHITELIST_External") AND mail-from-dictionary-match("Safe_Display_Name_Impersonators", 1)) {
-notify-copy ("$EnvelopeSender", "", "", "Recipient") ;                               
+quarantine_spoofed_protected_senders: if ((mail-from-dictionary-match("Protected_User_Names", 1)) OR (header-dictionary-match("Protected_User_Names","From", 1))) AND (sendergroup != "WHITELIST") AND (sendergroup != "DLP") AND (sendergroup != "RELAYLIST") AND (sendergroup != "Authorized_888Holdings") AND (mail-from-dictionary-match("Safe_Display_Name_Impersonators", 1)) {
+notify-copy ("$EnvelopeSender", "", "", "Notification") ;                               
 quarantine("Ext_888Holdings_Spoof");
                                          }
 
