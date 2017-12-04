b976a5bbe27d395d186a8d8ddb065e261082db49
yonivr <33428475+yonivr@users.noreply.github.com>
2017-12-04 15:01:19 +0200

    Update Update-CleanADUsers.ps1

--- Update-CleanADUsers.ps1
+++ Update-CleanADUsers.ps1
@@ -1,24 +1,30 @@
-#made by Yoni Rivkin 
-#Requires -module Posh-SSH
+<#
+made by Yoni Rivkin
+Uloade to SFTP list of active employees
+Requires -module Posh-SSH
+Requires -module AD
+#>
 
 #Parameters
-<#$SparkwareDC = "dcbuch01.sparkware.corp"#>
+#AD parameters
 $HrzBase = "OU=Users,OU=Herzliya,OU=Organization,DC=888holdings,DC=corp"
-#$KievBase = "OU=Users,OU=Kiev,OU=Organization,DC=888holdings,DC=corp"
+#SFTP parameters
 $SFTPUser = "Wifi-ADSync-SystemBO"
 $SFTPPass = "85eGOUN5" | ConvertTo-SecureString -asPlainText -Force
 $SFTPCred =  New-Object System.Management.Automation.PSCredential($SFTPUser,$SFTPPass)
 $SFTPServer = "sftp.888holdings.com"
+#general parameters
 $ADSyncRoot=(Get-Location).Path
-#//$date = (get-date -Format dd-MM-yy-mm-ss)
 #Get users
-#$SparkwareUsers = get-aduser -Filter * -Server $SparkwareDC -Properties extensionAttribute2 | Where-Object{($_.extensionAttribute2 -like "2*") -and ($_.enabled -eq $true)  } | Select-Object Name,GivenName,Surname,SamAccountName,extensionAttribute2,enabled
 $HrzUSers = get-aduser -Filter {enabled -eq $true} -SearchBase $HrzBase -Properties extensionAttribute2 | Where-Object{($_.extensionAttribute2 -like "2*") } | Select-Object Name,GivenName,Surname,SamAccountName,extensionAttribute2,enabled
-#$KievUsers = Get-ADUser -Filter * -SearchBase $KievBase -Properties extensionAttribute2 | Where-Object{($_.extensionAttribute2 -like "2*") -and ($_.enabled -eq $true)  } | Select-Object Name,GivenName,Surname,SamAccountName,extensionAttribute2,enabled
-$UsersToAdd = $SparkwareUsers + $HrzUSers + $KievUsers
+$UsersToAdd =  $HrzUSers
 $UserList = "$ADSyncRoot\CleanUsers.csv"
 $UsersToAdd | Export-csv $UserList
 #Upload users
 $UsersToAdd.Count
 $s = new-SFTPSession -credential $SFTPCred -ComputerName $SFTPServer
 Set-SFTPFile -SFTPSession $s -RemotePath / -LocalFile $UserList -Overwrite
+#close SFTP session
+Get-SFTPSession | Remove-SFTPSession
+#clean local list
+remove-item $UserList
