b8f73747cca0bc0aceb24bf3db964fdf462fe1d4
yonivr <33428475+yonivr@users.noreply.github.com>
2017-12-04 11:08:41 +0200

    Update Update-WifiYsers.ps1

--- Update-WifiYsers.ps1
+++ Update-WifiYsers.ps1
@@ -1,36 +1,40 @@
-#made by Yoni Rivkin 
-#Requires -module Posh-SSH
+<#
+made by Yoni Rivkin 
+Requires -module Posh-SSH
+Purpose: get list of users from clean and create users in dirty AD
+#>
 
 #Parameters
+#SFTP cred
 $SFTPUser = "Wifi-ADSync-SystemBO"
 $SFTPPass = "85eGOUN5" | ConvertTo-SecureString -asPlainText -Force
 $SFTPCred =  New-Object System.Management.Automation.PSCredential($SFTPUser,$SFTPPass)
 $SFTPServer = "sftp.888holdings.com"
+#File details
 $ScriptRoot = "C:\scripts\AD-Sync\v2"
 $CleanUsers = "CleanUsers.csv"
-$count=0
+#general param
+$count=0#count users to create
 $OU = "OU=WiFi,OU=Users,OU=888free.net,DC=888Free,DC=net"
 #connect to SFTP
 $s = new-SFTPSession -credential $SFTPCred -ComputerName $SFTPServer
-#get file from SFTP
-
-#get latest file containing cleanusers
+#get file from SFTP and overwrite old file
 
 "Downloading $File..."
 Get-SFTPFile -SFTPSession $s -RemoteFile $CleanUsers -LocalPath $ScriptRoot -Overwrite
 
-#create new 888free users
-    #iterate list of users
-    #check if user exists in Dirty AD
-        #If user doesnt exist create user
 $CleanADUsers = Import-Csv $ScriptRoot\$CleanUsers
 ($CleanADUsers).count
 
-$CleanADUsers | foreach-object {
+#create new 888free users
+$CleanADUsers | foreach-object { #iterate list of users
     $user = $_.SamAccountName
-	$UserExists = Get-ADObject -Filter {SamAccountName -eq $user}        
-	 if(!$UserExists)
-	{
+	$UserExists = Get-ADObject -Filter {SamAccountName -eq $user}  
+    #check if user exists in Dirty AD      
+	if(!$UserExists)
+	{ #If user doesnt exist create user
+        
+        #parameters
         $UserPass = "Hrz"+$_.extensionAttribute2
         $SecurePass = (ConvertTo-SecureString $UserPass -AsPlainText -force)
         $UserDisplayName = $_.name
@@ -38,7 +42,7 @@ $CleanADUsers | foreach-object {
         $Surname = $_.Surname
         
 		$UserPrinicpalName = $user + "@888free.net" 
-        
+        #user creation
        New-ADUser `
         -SamAccountName $user `
         -UserPrincipalName $UserPrinicpalName`
@@ -60,9 +64,12 @@ $CleanADUsers | foreach-object {
          ou:$OU
          
          "
-
-		
-		$count=$count + 1
+		$count=$count + 1#count users to create
 	}
  }
-$count
+$count#users created count
+
+#close SFTP session
+Get-SFTPSession | Remove-SFTPSession
+#remove user list
+Remove-Item $ScriptRoot\$CleanUsers
